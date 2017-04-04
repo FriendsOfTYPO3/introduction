a26382d4bda99ae0dd6b02119c60c9239d2f90ce
Benni Mack <benni@typo3.org>
2017-04-04 10:40:12 +0200

    [TASK] Updated content for TYPO3 v8 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,19 +12,19 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'The official Introduction Package',
-	'description' => 'This package delivers a new website (page tree) and shows all out-of-the-box features of TYPO3, and includes a theme based on Twitter Bootstrap 3, and a style editor to customize the design. Sample editors and backend groups are added',
+	'description' => 'This package delivers a new website (page tree) and shows all out-of-the-box features of TYPO3, and includes a theme based on Twitter Bootstrap 3, and a style editor to customize the design. Sample backend groups are added',
 	'category' => 'distribution',
-	'version' => '2.3.0',
+	'version' => '3.0.0',
 	'state' => 'stable',
 	'clearcacheonload' => 1,
 	'author' => 'Introduction Package Team',
 	'author_email' => '',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '7.6.0-8.9.99',
-            'bootstrap_package' => '7.0.0-7.99.99'
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    )
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '8.0.0-8.9.99',
+			'bootstrap_package' => '8.0.0-8.99.99'
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	)
 );
