622560df3b2a81fa0858a4bfd03daf8d14f424b7
Benjamin Kott <benjamin.kott@wfp2.com>
2017-06-03 23:23:51 +0200

    [TASK] Raise version dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,14 +14,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'The official Introduction Package',
 	'description' => 'This package delivers a new website (page tree) and shows all out-of-the-box features of TYPO3, and includes a theme based on Twitter Bootstrap 3, and a style editor to customize the design. Sample backend groups are added',
 	'category' => 'distribution',
-	'version' => '3.0.0',
+	'version' => '3.0.1',
 	'state' => 'stable',
 	'clearcacheonload' => 1,
 	'author' => 'Introduction Package Team',
 	'author_email' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.0.0-8.9.99',
+			'typo3' => '8.0.0-9.0.99',
 			'bootstrap_package' => '8.0.0-8.99.99'
 		),
 		'conflicts' => array(),
