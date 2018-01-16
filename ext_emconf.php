a7b327492e965857ea762bc668cacc93a59a3de0
Christian Kuhn <lolli@schwarzbu.ch>
2018-01-16 05:53:15 +0100

    [TASK] Bump version 3.1.0, depends on core >=9.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,14 +14,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'The official Introduction Package',
 	'description' => 'This package delivers a new website (page tree) and shows all out-of-the-box features of TYPO3, and includes a theme based on Twitter Bootstrap 3, and a style editor to customize the design. Sample backend groups are added',
 	'category' => 'distribution',
-	'version' => '3.0.1',
+	'version' => '3.1.0',
 	'state' => 'stable',
 	'clearcacheonload' => 1,
 	'author' => 'Introduction Package Team',
 	'author_email' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-9.5.99',
+			'typo3' => '9.1.0-9.5.99',
 			'bootstrap_package' => '9.1.0-9.1.99'
 		),
 		'conflicts' => array(),
