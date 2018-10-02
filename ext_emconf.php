7d9218496a727356b0e456de55daec7558becc75
Benni Mack <benni@typo3.org>
2018-10-02 10:12:46 +0200

    [TASK] Clean up ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "introduction".
-#
-# Auto generated 10-11-2015 15:19
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'The official Introduction Package',
 	'description' => 'This package delivers a new website (page tree) and shows all out-of-the-box features of TYPO3, and includes a theme based on Twitter Bootstrap 3, and a style editor to customize the design. Sample backend groups are added',
