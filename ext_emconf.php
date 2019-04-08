817308976515a77e630f0dd0261f35a4dee73b2d
jboesche <j.boesche@neusta.de>
2019-04-08 11:16:50 +0200

    [TASK] Add dev-master (T3 v10) compatibility and raise version to v4.0.2
    Releases: master
    Resolves: #22

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,20 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'The official Introduction Package',
 	'description' => 'This package delivers a new website (page tree) and shows all out-of-the-box features of TYPO3, and includes a theme based on Twitter Bootstrap 3, and a style editor to customize the design. Sample backend groups are added',
 	'category' => 'distribution',
-	'version' => '4.0.1',
+	'version' => '4.0.2',
 	'state' => 'stable',
 	'clearcacheonload' => 1,
 	'author' => 'Introduction Package Team',
 	'author_email' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '9.5.0-9.5.99',
-			'bootstrap_package' => '10.0.1-10.0.99'
-		),
-		'conflicts' => array(),
-		'suggests' => array(),
-	)
-);
+	'constraints' => [
+		'depends' => [
+			'typo3' => '9.5.0-10.0.99',
+			'bootstrap_package' => '9.5.0-10.0.99'
+        ],
+		'conflicts' => [],
+		'suggests' => [],
+    ]
+];
