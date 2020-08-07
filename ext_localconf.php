53dcc09297894fc0054e267062e6fbdf8cf96a14
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-08-08 00:10:43 +0200

    [FEATURE] Introduce a Sitepackage (to be amended)
    More changes:
    - add DDEV configuration for local development
    - add TYPO3 Console for local development
    - add VS Code settings for local development
    - add testing framework
    - add .editorconfig
    - enhance composer.json for local development
    - introduce v9 compatibility again

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,37 @@
 <?php
-defined('TYPO3_MODE') or die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
-  module.tx_form.settings.yamlConfigurations.100 = EXT:introduction/Configuration/Form/CustomFormSetup.yaml
-');
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+defined('TYPO3_MODE') or die();
+
+/**
+ * We must make sure that the feature toggle "felogin.extbase" is not active
+ * during the initial data import. Afterwards the update wizard can be used to
+ * migrate to the new Extbase implementation. Once the support for 9.5 is
+ * dropped this can be changed but then also the content element needs to be
+ * migrated.
+ */
+$registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Registry::class);
+
+if (
+    !$registry->get('introduction', 'feature.felogin.extbase.handled', false)
+    && \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\Features::class)
+        ->isFeatureEnabled('felogin.extbase')
+) {
+    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ConfigurationManager::class)
+        ->disableFeature('felogin.extbase');
+    $registry->set('introduction', 'feature.felogin.extbase.handled', true);
+}
+
+unset($registry);
