0e479ffc019448f32677bc630dc131be7106ffdd
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2021-08-23 21:44:30 +0200

    [TASK] Update TYPO3

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '10.0.0-10.4.99',
+			'typo3' => '10.4.0-10.4.99',
 			'bootstrap_package' => '11.0.0-11.5.99',
-			'impexp' => '10.0.0-10.4.99'
+			'impexp' => '10.4.0-10.4.99'
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
