f9623507d5600cb45c7326453f27ad992d1bfb51
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2021-08-31 12:24:58 +0200

    [FEATURE] Convert to sitepackage distribution

---
+++ Configuration/TCA/Overrides/100_sys_template.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+defined('TYPO3') || die();
+
+/***************
+ * TypoScript: Sitepackage
+ * This includes the full setup including all configurations
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'introduction',
+    'Configuration/TypoScript',
+    'Introduction: Sitepackage'
+);
