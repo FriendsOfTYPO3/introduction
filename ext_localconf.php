8e5619bac25036be2dd2253f0eeed08627d8a0d3
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2021-09-23 15:42:34 +0200

    [BUGFIX] Temporary move form to extension
    As long as the bug https://forge.typo3.org/issues/92974 is not fixed we
    move the form to the extension. Drawback, after the deinstalltion of the
    extension the form is not longer available.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,4 +3,5 @@ defined('TYPO3_MODE') or die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('
   module.tx_form.settings.yamlConfigurations.100 = EXT:introduction/Configuration/Form/CustomFormSetup.yaml
+  plugin.tx_form.settings.yamlConfigurations.100 = EXT:introduction/Configuration/Form/CustomFormSetup.yaml
 ');
